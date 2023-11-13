576c9ae1c5c16fa67a874b79ce0a1e0c71c44e09
ArtemZonov <artemzonov21@gmail.com>
2023-11-13 12:56:06 +0200

    added french, spanish, german and italian menu

--- app/src/main/java/com/azimuton/foreign/MainActivity.kt
+++ app/src/main/java/com/azimuton/foreign/MainActivity.kt
@@ -4,6 +4,7 @@ import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.View
 import com.azimuton.foreign.databinding.ActivityMainBinding
 import com.bumptech.glide.Glide
 import kotlinx.coroutines.CoroutineScope
@@ -22,6 +23,10 @@ class MainActivity : AppCompatActivity() {
     private var running = false
     private var openmainmenu = false
     private var openmainenglishmenu = false
+    private var openmainfrenchmenu = false
+    private var openmainspanishmenu = false
+    private var openmaingermanmenu = false
+    private var openmainitalianmenu = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -35,6 +40,10 @@ class MainActivity : AppCompatActivity() {
         Glide.with(this).asGif().load(R.drawable.flaggermany).into(binding.ivFlagGermany)
         Glide.with(this).asGif().load(R.drawable.flagitaly).into(binding.ivFlagItaly)
         Glide.with(this).asGif().load(R.drawable.flagengland).into(binding.ivMainEnglishMenu)
+        Glide.with(this).asGif().load(R.drawable.flagfrance).into(binding.ivMainFrenchMenu)
+        Glide.with(this).asGif().load(R.drawable.flagspain).into(binding.ivMainSpanishMenu)
+        Glide.with(this).asGif().load(R.drawable.flaggermany).into(binding.ivMainGermanMenu)
+        Glide.with(this).asGif().load(R.drawable.flagitaly).into(binding.ivMainItalianMenu)
         binding.ivMainMenu.setOnClickListener {
             if (openmainmenu) {
                 val dpValue = 150
@@ -59,7 +68,7 @@ class MainActivity : AppCompatActivity() {
         binding.tvMainClose.setOnClickListener { finishAffinity() }
         binding.ivMainEnglishMenu.setOnClickListener {
             if (openmainenglishmenu) {
-                val dpValue = 180
+                val dpValue = 145
                 val pxValue = TypedValue.applyDimension(
                     TypedValue.COMPLEX_UNIT_DIP, dpValue.toFloat(), resources.displayMetrics)
                 ObjectAnimator.ofFloat(binding.svEnglish, "translationX", pxValue).apply{
@@ -68,7 +77,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 openmainenglishmenu = false
             } else {
-                val dpValue = 180
+                val dpValue = 145
                 val pxValue = TypedValue.applyDimension(
                     TypedValue.COMPLEX_UNIT_DIP, dpValue.toFloat(), resources.displayMetrics)
                 ObjectAnimator.ofFloat(binding.svEnglish, "translationX", -pxValue).apply{
@@ -78,11 +87,151 @@ class MainActivity : AppCompatActivity() {
                 openmainenglishmenu = true
             }
         }
-        binding.ivFlagEngland.setOnClickListener {  }
-        binding.ivFlagFrance.setOnClickListener {  }
-        binding.ivFlagSpain.setOnClickListener {  }
-        binding.ivFlagGermany.setOnClickListener {  }
-        binding.ivFlagItaly.setOnClickListener {  }
+        binding.ivMainFrenchMenu.setOnClickListener {
+            if (openmainfrenchmenu) {
+                val dpValue = 145
+                val pxValue = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, dpValue.toFloat(), resources.displayMetrics)
+                ObjectAnimator.ofFloat(binding.svFrench, "translationX", pxValue).apply{
+                    duration = 1000
+                    start()
+                }
+                openmainfrenchmenu = false
+            } else {
+                val dpValue = 145
+                val pxValue = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, dpValue.toFloat(), resources.displayMetrics)
+                ObjectAnimator.ofFloat(binding.svFrench, "translationX", -pxValue).apply{
+                    duration = 1000
+                    start()
+                }
+                openmainfrenchmenu = true
+            }
+        }
+        binding.ivMainSpanishMenu.setOnClickListener {
+            if (openmainspanishmenu) {
+                val dpValue = 145
+                val pxValue = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, dpValue.toFloat(), resources.displayMetrics)
+                ObjectAnimator.ofFloat(binding.svSpanish, "translationX", pxValue).apply{
+                    duration = 1000
+                    start()
+                }
+                openmainspanishmenu = false
+            } else {
+                val dpValue = 145
+                val pxValue = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, dpValue.toFloat(), resources.displayMetrics)
+                ObjectAnimator.ofFloat(binding.svSpanish, "translationX", -pxValue).apply{
+                    duration = 1000
+                    start()
+                }
+                openmainspanishmenu = true
+            }
+        }
+        binding.ivMainGermanMenu.setOnClickListener {
+            if (openmaingermanmenu) {
+                val dpValue = 145
+                val pxValue = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, dpValue.toFloat(), resources.displayMetrics)
+                ObjectAnimator.ofFloat(binding.svGerman, "translationX", pxValue).apply{
+                    duration = 1000
+                    start()
+                }
+                openmaingermanmenu = false
+            } else {
+                val dpValue = 145
+                val pxValue = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, dpValue.toFloat(), resources.displayMetrics)
+                ObjectAnimator.ofFloat(binding.svGerman, "translationX", -pxValue).apply{
+                    duration = 1000
+                    start()
+                }
+                openmaingermanmenu = true
+            }
+        }
+        binding.ivMainItalianMenu.setOnClickListener {
+            if (openmainitalianmenu) {
+                val dpValue = 145
+                val pxValue = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, dpValue.toFloat(), resources.displayMetrics)
+                ObjectAnimator.ofFloat(binding.svItalian, "translationX", pxValue).apply{
+                    duration = 1000
+                    start()
+                }
+                openmainitalianmenu = false
+            } else {
+                val dpValue = 145
+                val pxValue = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, dpValue.toFloat(), resources.displayMetrics)
+                ObjectAnimator.ofFloat(binding.svItalian, "translationX", -pxValue).apply{
+                    duration = 1000
+                    start()
+                }
+                openmainitalianmenu = true
+            }
+        }
+
+        binding.ivFlagEngland.setOnClickListener {
+            binding.svEnglish.visibility = View.VISIBLE
+            binding.svFrench.visibility = View.GONE
+            binding.svSpanish.visibility = View.GONE
+            binding.svGerman.visibility = View.GONE
+            binding.svItalian.visibility = View.GONE
+            binding.ivMainEnglishMenu.visibility = View.VISIBLE
+            binding.ivMainFrenchMenu.visibility = View.GONE
+            binding.ivMainSpanishMenu.visibility = View.GONE
+            binding.ivMainGermanMenu.visibility = View.GONE
+            binding.ivMainItalianMenu.visibility = View.GONE
+        }
+        binding.ivFlagFrance.setOnClickListener {
+            binding.svFrench.visibility = View.VISIBLE
+            binding.svEnglish.visibility = View.GONE
+            binding.svSpanish.visibility = View.GONE
+            binding.svGerman.visibility = View.GONE
+            binding.svItalian.visibility = View.GONE
+            binding.ivMainFrenchMenu.visibility = View.VISIBLE
+            binding.ivMainEnglishMenu.visibility = View.GONE
+            binding.ivMainSpanishMenu.visibility = View.GONE
+            binding.ivMainGermanMenu.visibility = View.GONE
+            binding.ivMainItalianMenu.visibility = View.GONE
+        }
+        binding.ivFlagSpain.setOnClickListener {
+            binding.svEnglish.visibility = View.GONE
+            binding.svFrench.visibility = View.GONE
+            binding.svSpanish.visibility = View.VISIBLE
+            binding.svGerman.visibility = View.GONE
+            binding.svItalian.visibility = View.GONE
+            binding.ivMainEnglishMenu.visibility = View.GONE
+            binding.ivMainFrenchMenu.visibility = View.GONE
+            binding.ivMainSpanishMenu.visibility = View.VISIBLE
+            binding.ivMainGermanMenu.visibility = View.GONE
+            binding.ivMainItalianMenu.visibility = View.GONE
+        }
+        binding.ivFlagGermany.setOnClickListener {
+            binding.svEnglish.visibility = View.GONE
+            binding.svFrench.visibility = View.GONE
+            binding.svSpanish.visibility = View.GONE
+            binding.svGerman.visibility = View.VISIBLE
+            binding.svItalian.visibility = View.GONE
+            binding.ivMainEnglishMenu.visibility = View.GONE
+            binding.ivMainFrenchMenu.visibility = View.GONE
+            binding.ivMainSpanishMenu.visibility = View.GONE
+            binding.ivMainGermanMenu.visibility = View.VISIBLE
+            binding.ivMainItalianMenu.visibility = View.GONE
+        }
+        binding.ivFlagItaly.setOnClickListener {
+            binding.svEnglish.visibility = View.GONE
+            binding.svFrench.visibility = View.GONE
+            binding.svSpanish.visibility = View.GONE
+            binding.svGerman.visibility = View.GONE
+            binding.svItalian.visibility = View.VISIBLE
+            binding.ivMainEnglishMenu.visibility = View.GONE
+            binding.ivMainFrenchMenu.visibility = View.GONE
+            binding.ivMainSpanishMenu.visibility = View.GONE
+            binding.ivMainGermanMenu.visibility = View.GONE
+            binding.ivMainItalianMenu.visibility = View.VISIBLE
+        }
     }
     private fun runTimer() {
         coroutine?.cancel()
@@ -101,5 +250,10 @@ class MainActivity : AppCompatActivity() {
                 delay(1000)
             }
         }
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finishAffinity()
     }
 }
